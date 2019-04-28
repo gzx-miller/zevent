64912cf0acf721c6ec03700f51eb76ae95bcdfa9
guozhenxing <guozhenxing@qiyi.com>
2019-04-28 14:35:01 +0800

    remove main

--- example/test.js
+++ example/test.js
@@ -5,22 +5,18 @@ let onEv1 = (a, b)=>{
     console.log("event coming: ev-1", a, b);
 }
 
-let main = () => {
-    let event = new ZEvent();
+let event = new ZEvent();
 
-    console.log("event on: ev-1, onEv1");
-    event.on(ev1, onEv1);
+console.log("event on: ev-1, onEv1");
+event.on(ev1, onEv1);
 
-    console.log("event send: ev-1 123, abc");
-    event.send(ev1, 123, "abc");
+console.log("event send: ev-1 123, abc");
+event.send(ev1, 123, "abc");
 
-    setTimeout(() => {
-        console.log("event off: ev-1, onEv1");
-        event.off(ev1, onEv1);
+setTimeout(() => {
+    console.log("event off: ev-1, onEv1");
+    event.off(ev1, onEv1);
 
-        console.log("event send: ev-1 456, def");
-        event.send(ev1, 456, "def");
-    }, 200);
-}
-
-main();
+    console.log("event send: ev-1 456, def");
+    event.send(ev1, 456, "def");
+}, 200);
